6ede4f6a02b91db6f4ed015268d5019eefe0ba7f
oreo91 <73924170+oreo91@users.noreply.github.com>
2022-05-14 20:46:57 -0400

    Add files via upload

---
+++ eclipse-workspace/helloWorld/src/helloWorld/Main.java
@@ -0,0 +1,24 @@
+package helloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		String txt = "abcdefghijklmnopqrstuvwxyz";
+		System.out.println("The length of the txt string is: "+ txt.length());
+		//above code checks for the length in a String
+		System.out.println(txt.indexOf('i'));
+		//prints the location of "i" in the txt String
+		 double myDouble = 9.7d;
+		 int myInt =(int) myDouble;
+		 System.out.println(myDouble); 
+		 int sum1 = 100+50;
+		 int sum2 = sum1 + 250;
+		 System.out.println(sum2);
+		
+
+	}
+
+}
+ 
